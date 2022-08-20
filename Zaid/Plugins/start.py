0cbbacb48247b25be7f162f7c12daf9c1cbba8ce
KARTIK RAJ OFFICIAL <104128091+KartikRajOfficials@users.noreply.github.com>
2022-08-20 17:18:37 +0530

    Update start.py

--- Zaid/Plugins/start.py
+++ Zaid/Plugins/start.py
@@ -26,7 +26,7 @@ async def play(_, message: Message):
         user_name = message.from_user.first_name
         rpk = "["+user_name+"](tg://user?id="+str(user_id)+")" 
         await app.send_message(message.chat.id,
-            text=f"Hello {rpk}!\n\nThis is Zaid Vc Player.\nI play music on Telegram's Voice Chats.\n\nRead commands List or Start using me.",
+            text=f"Hello {rpk}!\n\nThis is an Music Bot.\nI play music on Telegram's Voice Chats and also can download video from YouTube in audio or in video format.\n\nRead commands List or Start using me.",
             parse_mode="markdown",
             reply_markup=start_keyboard,
             reply_to_message_id=message.message_id
@@ -57,7 +57,7 @@ async def play(_, message: Message):
 📎 **Channel Link:** [Visit From Here]({x["channel_url"]})
 🔗 **Link:** [Link]({x["webpage_url"]})
 
-⚡️ __Searched Powered By Zaid Vc Player__"""
+⚡️ __Searched Powered by Bot__"""
             link = (x["webpage_url"])
             buttons = personal_markup(link)
             userid = message.from_user.id
